fe8a4f2d948f9f726936a74616e22b1051eadce5
Sigvard224 <78226097+Sigvard224@users.noreply.github.com>
2025-04-28 13:55:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
+         $_SESSION['user_name'] = $user['name'];
         $_SESSION['is_admin'] = $user['is_admin']; // sätt även detta direkt
 
         if ($user['is_admin'] == 1) {
